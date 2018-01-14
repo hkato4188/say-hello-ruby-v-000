5ffae2074988ccb12301071f5dd435fa91665f15
Hiroki Kato <hkato@sas.upenn.edu>
2018-01-14 03:28:06 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,5 +1,6 @@
 # Build your say_hello method here
-def say_hello (name = "Ruby Programmer")
+def say_hello (name = "Ruby programmer")
+  puts "what is your name?"
   name = gets
-  puts "hello, #{name}!""
+  puts "hello #{name}!""
 end
